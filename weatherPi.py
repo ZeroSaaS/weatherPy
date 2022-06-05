f02db363400022f1839d2ac445d285c72dc1af38
Greg Hamelin <103612690+ZeroSaaS@users.noreply.github.com>
2022-06-05 09:01:49 -0400

    Update weatherPi.py

--- weatherPi.py
+++ weatherPi.py
@@ -3,19 +3,34 @@ import requests
 import json
 
 #weatherbit API request for current weather in a particular zip code and save JSON to variable 'data'
-#replace YOUR_ZIP_CODE with your 5 digit zip code. replace YOUR_WEATHERBIT_KEY with your key from weatherbit.io.  
 URL = "https://api.weatherbit.io/v2.0/current?&postal_code=YOUR_ZIP_CODE&country=US&key=YOUR_WEATHERBIT_KEY"
 data = []
 r = requests.get(url = URL)
 data = r.json()
 
 #extract 'temp' in 'data' and convert to fahrenheit
-temp = ((data['data'][0]['temp']) * (9/5)) + 32
+temp = round(((data['data'][0]['temp']) * (9/5)) + 32, 0)
+#extract weather 'description' in data
+desc = ""
+desc = data['data'][0]['weather']['description']
 
-#get icon image from weatherbit using URL
-icon = str(data['data'][0]['weather']['icon'])
-icon_URL = "https://www.weatherbit.io/static/img/icons/" + icon + ".png"
+#extract 'rh' relative humidity % in 'data'
+rh = data['data'][0]['rh']
 
-#print the temperature to the screen and the URL of the weather icon to verify it is working properly.
-print(f"The temperature is: {temp} outside")
-print(icon_URL)
+#extract 'aqi' air quality index from 'data'
+aqi = data['data'][0]['aqi']
+
+#extract 'pres' barometric pressure from 'data'
+pres = data['data'][0]['pres']
+
+#extract 'sunset' sunset time from 'data'
+sunset = data['data'][0]['sunset']
+
+#extract 'sunrise' sunrise time from 'data'
+sunrise = data['data'][0]['sunrise']
+
+#extract 'uv' uv index from 'data'
+uv = data['data'][0]['uv']
+
+#extract 'precip' precipitation from 'data'
+precip = data['data'][0]['precip']
